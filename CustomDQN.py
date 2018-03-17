8d07c42342311d3bb7fa0808ee30893d5d96cadc
Nathan Shepherd <nateshep@umich.edu>
2018-03-16 23:38:48 -0400

    Starting CustomDQN

---
+++ CustomDQN.py
@@ -0,0 +1,174 @@
+import gym
+#import universe
+
+import random
+import numpy as np
+
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+
+
+class Memory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.container = []
+
+    def get_sample(self, size):
+        return random.sample(self.container, size)
+    
+    def push_sample(self,state, action, reward, state_next, done):
+        self.container.append([state, action, reward, state_next, done])
+    
+class DQN:
+    def __init__(
+            self,
+            action_space=None,
+            observation_space=None,
+            learning_rate=0.005,
+            reward_decay=0.9,
+            random_action_chance=0.9,
+            #replace_target_iter=500,
+            memory_size=10000,
+            batch_size=32,
+            e_greedy_increment=None,
+            hidden=None,
+            output_graph=False,
+    ):
+        self.action_space = action_space
+        self.observation_space = observation_space
+        self.LR = learning_rate
+        self.reward_decay = 0.9
+        self.epsilon = random_action_chance
+        #self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.epsilon_increment = e_greedy_increment
+
+        self.hidden = None
+        self.output_graph = None
+
+        self.gamma = 0.9
+        self.memory = Memory(memory_size)
+
+    def init_model(self, observation_space, nb_actions):#TODO: parameterize
+        self.action_space = nb_actions
+        self.observation_space = observation_space
+        
+        model = Sequential()
+        model.add(Dense(20, input_shape=(1,) + observation_space))
+        model.add(Flatten())
+        model.add(Dense(16))
+        model.add(Activation('relu'))
+        model.add(Dense(16))
+        model.add(Activation('relu'))
+        model.add(Dense(16))
+        model.add(Activation('relu'))
+        model.add(Dense(nb_actions))
+        model.add(Activation('linear'))
+
+        model.compile(loss='mse', optimizer='adam',
+                      metrics=['accuracy'])
+        print(model.summary())
+
+        self.model = model
+
+    def _take_random_action(self):
+        return random.randint(0, self.action_space)
+    
+    def store_transition(self, state, action, reward, state_next, done):
+        self.memory.push_sample(state, action, reward, state_next, done)
+
+    def get_action(self, state):
+        # decrement epsilon via: e = e_min + (e_max - e_min)^(lambda*time)
+        # where lambda is the decay factor
+        if random.random() < self.epsilon:
+            return self._take_random_action()
+        else:
+            #add another dimension to state (i.e. batch size is one)
+            state = np.expand_dims(state, axis=0)
+            state = np.stack((state,), axis=1)
+            #state = np.array(state)
+            print(state.shape)
+            return np.argmax(self.model.predict(state))
+
+    def siraj_train(self):
+        transitions = self.memory.get_sample(self.batch_size)
+
+        inputs_shape = (self.batch_size,) + self.observation_space
+        inputs = np.zeros(inputs_shape)
+        targets = np.zeros((self.batch_size, self.action_space))
+
+        for i in range(len(transitions)):
+            trans = transitions[i]
+            state = trans[0];
+            action = trans[1];
+            reward = trans[2];
+            state_new = trans[3]
+            done = trans[4]
+
+            inputs[i] = state
+            targets[i] = self.get_action(state)
+            Q_sa = self.get_action(state_new)
+
+            if done:
+                targets[i, action] = reward
+            else:
+                targets[i, action] = reward + self.gamma * np.max(Q_sa)
+
+        #train network to output Q function
+        self.model.train_on_batch(inputs, targets)
+        
+
+    def train(self):
+        #get training samples from memory
+        transitions = self.memory.get_sample(self.batch_size)
+        
+        states = np.array([ trans[0] for trans in transitions ])
+        states_ = np.array([ trans[3] for trans in transitions ])
+
+        pred = self.model.predict(states)
+        pred_ = self.model.predict(states_)
+
+        x = np.zeros((self.batch_size, self.observation_space))
+        y = np.zeros((self.batch_size, self.action_space))
+
+        for i in range(len(transitions)):
+            trans = transitions[i]
+            state = trans[0];
+            action = trans[1];
+            reward = trans[2];
+            state_new = trans[3]
+            done = trans[4]
+            
+            target = pred[i]
+            if done:
+                target[action] = reward
+            else:
+                target[action] = reward + self.gamma * np.amax(pred_[i])
+
+            x[i] = state
+            y[i] = target
+        
+        self.model.fit(x, y, batch_size=self.batch_size,
+                             nb_epoch=1, verbose=verbose) 
+        
+'''
+if __name__ == "__main__":
+    agent = DQN()
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lunarLander-CustomDQN.py
@@ -0,0 +1,184 @@
+#Solve the Lunar Lander Game enviornment
+
+import gym
+import random
+import numpy as np
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import median, mean
+from collections import Counter
+
+from CustomDQN import DQN
+
+LR = 1e-2
+env = gym.make("LunarLander-v2")
+goal_steps = 90#2000
+score_requirement = -150#TODO: determine good value
+initial_games = 100#10000
+
+action_space = 4
+
+def create_random_samples(init_obs):
+    # [state, action, reward, state_new, done]
+    training_data = []
+    scores = []
+    
+    # just the scores that met threshold:
+    accepted_scores = []
+    
+    for i in range(initial_games):
+        if i % 10 == 0:
+            print('Observing random samples: {}%'.format(i*100/initial_games))
+            
+        score = 0
+        game_memory = []
+        state = init_obs
+
+        for _ in range(goal_steps):
+            action = random.randrange(0, action_space)
+            state_new, reward, done, info = env.step(action)
+                
+            game_memory.append([state, action, reward, state_new, done])
+            score += reward
+            
+            if done: break
+
+        # IF our score >= threshold, we'd like to save [action, obs] pairs
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            
+            for data in game_memory:
+                training_data.append(data)
+                '''
+                action_vect = np.zeros(action_space)
+                # convert to one-hot (this is the output layer for our neural network)
+                action_vect[data[1]] = 1
+                # [state, action, reward, state_new, done]
+                training_data.append([(action_vect if i == 1 else elem) for i, elem in enumerate(data)])
+                '''
+                
+        init_obs = env.reset()
+        scores.append(score)
+
+    training_data_save = np.array(training_data)
+    #np.save('lunar_lander_training_data.npy',training_data_save)
+    
+    print('Average accepted score:',mean(accepted_scores))
+    print('Median score for accepted scores:',median(accepted_scores))
+    print(Counter(accepted_scores))
+    
+    return np.array(training_data)
+
+def neural_network_model(input_size):
+
+    network = input_data(shape=[None, input_size, 1], name='input')
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.9)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.9)
+
+    network = fully_connected(network, 512, activation='relu')
+    network = dropout(network, 0.9)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.9)
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.9)
+
+    network = fully_connected(network, action_dims, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=LR,
+                         loss='categorical_crossentropy', name='targets')
+    model = tflearn.DNN(network, tensorboard_dir='log')
+
+    return model
+
+def train_model(training_data, model=False):
+
+    X = np.array([i[0] for i in training_data]).reshape(-1,len(training_data[0][0]),1)
+    y = [i[1] for i in training_data]
+
+    if not model:
+        model = neural_network_model(input_size = len(X[0]))
+    
+    model.fit({'input': X}, {'targets': y}, n_epoch=3,
+              snapshot_step=500, show_metric=True, run_id='openai_learning')
+    return model
+
+if __name__ == "__main__":
+    Agent = DQN()
+    
+    training_data = create_random_samples(env.reset())
+    for datum in training_data:
+        s, a, r, s_, done = datum
+        Agent.store_transition(s, a, r, s_, done)
+
+    Agent.init_model(training_data[0][0].shape, action_space)
+    Agent.siraj_train()
+    #model = train_model(training_data)
+
+    scores = []
+    choices = []
+    for each_game in range(10):
+        score = 0
+        game_memory = []
+        prev_obs = []
+        env.reset()
+        for _ in range(goal_steps):
+            env.render()
+
+            if len(prev_obs)==0:
+                action = random.randrange(0,action_dims)
+            else:
+                action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs),1))[0])
+
+            choices.append(action)
+                
+            new_observation, reward, done, info = env.step(action)
+        
+            prev_obs = new_observation
+            game_memory.append([new_observation, action])
+            score+=reward
+            if done: break
+
+        scores.append(score)
+
+    print('Average Score:',sum(scores)/len(scores))
+    print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),
+                                        choices.count(0)/len(choices)))
+    print(score_requirement)
+
+
+'''
+def some_random_games_first():
+    # Each of these is its own game.
+    for episode in range(5):
+        env.reset()
+        print("\nactions: ")
+        # this is each frame, up to 200...but we wont make it that far.
+        for t in range(200):
+            # This will display the environment
+            # Only display if you really want to see it.
+            # Takes much longer to display it.
+            env.render()
+
+            # This will just create a sample action in any environment.
+            # In this environment, the action can be 0 or 1, which is left or right
+            action = env.action_space.sample()
+            print(action, end="")
+            
+            # this executes the environment with an action,
+            # and returns the observation of the environment,
+            # the reward, if the env is over, and other info.
+            observation, reward, done, info = env.step(action)
+           
+            
+            if done:
+                break
+
+create_random_samples()
+#some_random_games_first()
+'''
