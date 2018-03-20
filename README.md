b081c3f777c22146ee23fcc432776bdcab064199
nathanShepherd <nateshep@umich.edu>
2018-03-20 01:40:59 -0400

    Update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Implimentation of the Intelligent Human-Computer interface. This RL agent is detailed extensively in the accompanied research paper.
 
-The answer that you seek can be found at Intelligent-Interface/keras-rl-master/rl/core.py
+TODO:
+	Impliment Actor-Critic system that takes observations as a continous 2D space and outputs actions in a continuous 2D space
+	Impliment mwob_Actor which can move the mouse (up, down, left, right) at each time step using DQN
--- mwob_Agent_CustomDQN.py
+++ mwob_Agent_CustomDQN.py
@@ -6,10 +6,12 @@
  ###########################################################
 # Source Code: https://github.com/nathanShepherd/Intelligent-Interface
 
-
 import gym
 import universe
 import numpy as np
+
+import random
+from CustomDQN_March_18 import DQN
 
 def observe_and_take_random_action(obs):
   # obs is raw (768,1024,3) uint8 screen .
@@ -47,7 +49,7 @@ def observe_and_take_random_action(obs):
   #return list of vnc events
   return action
 
-def get_env_space(env):
+def get_obs_space(env):
   observation = env.reset()
   done = False
 
@@ -71,7 +73,55 @@ def get_env_space(env):
   #TODO: decide action space from env name
   action = np.array([[0,0,0],[0,0,1],[0,0,0]])
 
-  return crop.shape, action.shape
+  return crop.shape
+
+def create_random_samples(init_obs, env):
+    # [state, action, reward, state_new, done]
+    training_data = []
+    scores = []
+    
+    # just the scores that met threshold:
+    accepted_scores = []
+    
+    for i in range(initial_games):
+        if i % 10 == 0:
+            print('Observing random samples: {}%'.format(i*100/initial_games))
+            
+        score = 0
+        game_memory = []
+        state = init_obs
+
+        for _ in range(goal_steps):
+            #TODO: Take actions across continuous 2D plane
+            action = random.randrange(0, action_space)
+            if action == action_space:
+                print("FOUND")
+            #print("ACTION::::", action, type(action))
+            state_new, reward, done, info = env.step(action)
+                
+            game_memory.append([state, action, reward, state_new, done])
+            score += reward
+            
+            if done: break
+
+        # IF our score >= threshold, we'd like to save [action, obs] pairs
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            
+            for data in game_memory:
+                training_data.append(data)
+                
+        init_obs = env.reset()
+        scores.append(score)
+
+    training_data_save = np.array(training_data)
+    #np.save('lunar_lander_training_data.npy',training_data_save)
+    
+    print('Average accepted score:',mean(accepted_scores))
+    print('Median score for accepted scores:',median(accepted_scores))
+    print("Number of acccepted scores:", len(accepted_scores))
+    
+    return np.array(training_data)
 
 def get_CustomDQN_Agent(env, action_shape, observation_space):
   print('\n\n}{}{}}{}{}{}{}{}{}{}{}{}{}{')
@@ -85,7 +135,17 @@ def get_CustomDQN_Agent(env, action_shape, observation_space):
   print("Length of unwrapped action space:", nb_actions)
   print('}{}{}}{}{}{}{}{}{}{}{}{}{}{')
 
-
+'''Take in coordinates on the screen, return action as a list of VNC events'''
+def coord_to_event(xcoord, ycoord):
+  # Line 1: Move to x, y
+  # Line 2 & 3: Click at (x, y)
+  # TODO: make actions modular with respect to the task required
+  #       consider using the env name to decide which class of
+  #       set of actions would make the most sense for the context
+  action = [universe.spaces.PointerEvent(xcoord, ycoord, 0),
+            universe.spaces.PointerEvent(xcoord, ycoord, 1),
+            universe.spaces.PointerEvent(xcoord, ycoord, 0)]
+  return action
 
 def random_game_loop(env):
   observation = env.reset()
@@ -97,23 +157,35 @@ def random_game_loop(env):
     print('####\nReward:', reward_n,'\n')
     print("####\nInfo:", info, '\n')
     env.render()
+
+#~~~~~~~~~~~[  MAIN  ]~~~~~~~~~~~#
+    
+#initialize game environment
+env = gym.make('wob.mini.ClickButton-v0')
+goal_steps = 1000#2000
+score_requirement = -200#-150
+initial_games = 50#10000, 250 adequite
+num_training_games = 100#>1000
+
+# x, y coordinates for Click games
+action_space = 2
   
 if __name__ == "__main__":
-  #initialize game environment
-  env = gym.make('wob.mini.ClickButton-v0')
-
   # automatically creates a local docker container
   env.configure(remotes=1, fps=5, vnc_driver='go',
               vnc_kwargs={'encoding':'tight', 'compress_level': 0,
                           'fine_quality_level': 100, 'subsample_level': 0})
 
-  #random_game_loop(env)
-  obs_space, act_space = get_env_space(env)
   
+  obs_space= get_obs_space(env)
   print("%%%%%\nThe observation space is:",obs_space,"\n%%%%%")
-  print("%%%%%\nThe action space is:",act_space,"\n%%%%%")
+  print("%%%%%\nThe action space is: continuous 2D\n%%%%%")
 
-  use_keral_rl(env, act_space, obs_space)
+  initial_observation = env.reset()
+  training_data = create_random_samples(initial_observation, env)
+  
+
+  Agent = get_CustomDQN_Agent(env, act_space, obs_space)
 
 
 
